9e9ea15928b979ca220b744ec3ecfa0c290b6587
Matt Eichsteadt <matteichsteadt@yahoo.com>
2016-10-21 14:11:07 -0700

    have the result display with a picture.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
+//=========================== Global Variables ==============================================
+var img, result, append = "";
+var rubyImg = '<img class="codeImg" src="img/ruby.png" alt="ruby picture">';
+var cSharpImg = '<img class="codeImg" src="img/cSharp.png" alt="c# picture">';
+var cssImg = '<img class="codeImg" src="img/css.png" alt="css picture">';
+
 //=========================== Back end ======================================================
-function result(arr) {
+function scoreQuiz(arr) {
   var cSharp = 0, css=0, ruby=0;
   for(var i=0;i<arr.length;i++) {
     if(arr[i] === "cSharp") {
@@ -13,27 +19,75 @@ function result(arr) {
     }
   }
   if(css === ruby && css === cSharp) {
-    return "Ruby, C#, and CSS";
+    result = "It's dead even.";
+    img = rubyImg + cSharpImg + cssImg;
+    append = "Looks like this quiz didn't help much."
   }
   else if(css > ruby && css > cSharp) {
-    return "CSS";
+    result = "You got CSS";
+    img = cssImg;
+    append = "";
   }
   else if (ruby > cSharp && ruby > css) {
-    return "Ruby";
+    result = "You got Ruby";
+    img = rubyImg;
+    append = "";
   }
   else if(cSharp > ruby && cSharp > css){
-    return "C#";
+    result = "You got C#";
+    img = cSharpImg;
   }
   else if(css === ruby) {
-    return "CSS and Ruby";
+    result = "It's tied between CSS and Ruby";
+    img = cssImg + rubyImg;
   }
   else if(css === cSharp) {
-    return "CSS and C#";
+    result = "It's tied between CSS and C#";
+    img = cssImg + cSharpImg;
+    append = "";
   }
   else if(ruby === cSharp) {
-    return "Ruby and C#";
+    result = "It's tied between Ruby and C#";
+    img = rubyImg + cSharpImg;
+    append = "";
   }
-  console.log("css:" + css, 'ruby:' + ruby, 'c#:' + cSharp);
+}
+
+function img(arr) {
+  var cSharp = 0, css=0, ruby=0;
+  for(var i=0;i<arr.length;i++) {
+    if(arr[i] === "cSharp") {
+      cSharp++;
+    }
+    else if(arr[i] === "ruby") {
+      ruby++;
+    }
+    else if (arr[i] === "css") {
+      css++;
+    }
+  }
+  if(css === ruby && css === cSharp) {
+    return "It's dead even. Looks like this quiz didn't help much.";
+    img = rubyImg;
+  }
+  else if(css > ruby && css > cSharp) {
+    return "You got CSS";
+  }
+  else if (ruby > cSharp && ruby > css) {
+    return "You got Ruby";
+  }
+  else if(cSharp > ruby && cSharp > css){
+    return "You got C#";
+  }
+  else if(css === ruby) {
+    return "It's tied between CSS and Ruby";
+  }
+  else if(css === cSharp) {
+    return "It's tied between CSS and C#";
+  }
+  else if(ruby === cSharp) {
+    return "It's tied between Ruby and C#";
+  }
 }
 
 function checkInput(arr) {
@@ -79,7 +133,10 @@ $(function() {
       $('.try-again').show();
     }
     else {
-      $('#result').append('<h1>You should take ' + result(answers) +'</h1>');
+      scoreQuiz(answers);
+      $('#result').append('<h1>' + result + '</h1>');
+      $('#result').append('<h4>' + append + '</h4>')
+      $('#result').append(img);
       $('#result').show();
       $('#quiz').hide();
     }
@@ -89,7 +146,13 @@ $(function() {
   $('#lame').click(function() {
     $('.lame').show();
     $('#lame').attr('disabled', 'true');
+
   });
 
-
-})
+  $('h1').click(function() {
+    $('#quiz').hide();
+    //$('#result').append('<h1>You should take ' + result(answers) +'</h1>');
+    $('#result').append(cssImg);
+    $('#result').show();
+  });
+});
